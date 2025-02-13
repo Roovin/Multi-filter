2a1578bbe473409a031ed405cd9d82fa5b79c73c
Roovin <roovinbansal@gmail.com>
2025-02-13 17:49:18 +0530

    Update pattern

--- pattern.js
+++ pattern.js
@@ -1,7 +1,7 @@
 const sectionWrap = document.querySelector('.patternWarp');
 if(sectionWrap) {
     //Right Star Pattern
-    const starPattern = sectionWrap.querySelector('.right-star-pattern');
+    const starPattern = sectionWrap.querySelector('.left-star-pattern');
     if(starPattern) {
         let pattern = ""; 
         for(var i = 1; i <= 5; i++) {
@@ -18,16 +18,130 @@ if(sectionWrap) {
     //Reverse Right Star Pattern
     const revStarPattern = sectionWrap.querySelector('.reverse-right-pattern');
     if(revStarPattern) {
-        let pattern = "";
+        var pattern = "";
         for(var i = 1; i <= 5; i++) {
-            pattern =+ `<span>  </span>`;
-            for(var j = i; j < 5; j++) {
+            for(var j = i; j <= 5; j++) {
                 pattern += `<span>*</span>
-                <span> </span>`;
+                <span>  </span>`;
             }
             pattern += `</br>`;
         }
-        revStarPattern.innerHTML = pattern
+        revStarPattern.innerHTML = pattern;
+
+      
+
     }
 
+    //Right Star pattern
+    const rightPattern = sectionWrap.querySelector('.right-pattern');
+    if(rightPattern) {
+        var patternR = "";
+        for (var i = 0; i < 5; i++) {
+            for(var j = i+1; j < 5; j++) {
+                patternR += `<span class="space"> </span>`;
+            }
+            for(var k = 0; k <= i; k++) {
+                patternR += `<span>*</span>
+                            <span> </span>`;
+            }
+            patternR += `</br>`;
+        }
+        rightPattern.innerHTML = patternR
+    }
+
+    //Right Reverse Star pattern
+    const rightRevPattern = sectionWrap.querySelector('.right-rev-pattern');
+    if(rightRevPattern) {
+        var patternR = "";
+        for (var i = 0; i < 5; i++) {
+            for(var j = i; j < 5; j++) {
+                patternR += `<sapn> </span>
+                            <span>*</span>`;
+            }
+            patternR += `</br>`;
+            for(var k = 0; k <= i; k++) {
+                patternR += `<span class="space"></span>`;
+            }
+           
+        }
+        rightRevPattern.innerHTML = patternR
+    }
+
+    //Triangle Star Pattern 
+    const trianglePattern = sectionWrap.querySelector('.triangle-pattern');
+    if(trianglePattern) {
+        var triangle = "";
+        for (var i = 0; i < 5; i++) {
+            for (var j = i + 1; j < 5; j++) {
+                triangle += `<span class="space"></span>`;
+            }
+            for (var k = 0; k <= i; k++) {
+                triangle += `<span>*</span>
+                <span class="space"></span>`;
+            }
+            for (var l = i+1; l < 5; l++) {
+                triangle += `<span class="space"></span>`;
+            }
+            triangle += `</br>`;
+        }
+        trianglePattern.innerHTML = triangle 
+    }
+
+    // Reverse Triangle Pattern
+    const revTrianglePattern = sectionWrap.querySelector('.rev-triangle-pattern');
+    if(revTrianglePattern) {
+        let revTriangle = "";
+        for(var i = 0; i < 5; i++) {
+            for(var j = i; j < 5; j++) {
+                revTriangle += `<span>*</span>
+                                    <span class="space"></span>`;
+            }
+            revTriangle += `</br>`;
+            for(var k = 0; k <= i; k++) {
+                revTriangle += `<span class="space"></span>`;
+            }
+        }
+        revTrianglePattern.innerHTML = revTriangle
+    }
+
+    //Left Reverse Triangle Pattern
+    const leftTrianglePattern = sectionWrap.querySelector('.left-triangle-pattern');
+    if(leftTrianglePattern) {
+        let leftTriangle = "";
+        for(var i = 0; i < 5; i++) {
+            for(var j = 0; j <= i; j++) {
+                leftTriangle += `<span>*</span>
+                                <span></span>`;
+            }
+            for(var k = i+1; k < 5; k++) {
+                leftTriangle += `<span class="space"></span>`
+            }
+            leftTriangle += `</br>`;
+        }
+        for(var l = 0; l < 5; l++) {
+            for(var h = l+1; h < 5; h++) {
+                leftTriangle += `<span>*</span>
+                                <span></span>`;
+            }
+            leftTriangle += `</br>`;
+        }
+        leftTrianglePattern.innerHTML = leftTriangle
+    }
+
+    //Paramid Triangle Pattern
+    const paraTrianglePattern = sectionWrap.querySelector('.paramid-triangle-pattern');
+    if(paraTrianglePattern) {
+        let paraTriangle = ""
+        for(let i = 1; i < 8; i++) {
+            for(let j = i+1; j <= 6; j++) {
+                paraTriangle += `<span class="space"></span>`
+            }
+            for(let k = i*2; k <= 8; k++) {
+                paraTriangle += `<span>*</span>
+                                <span class="space"></span>`
+            }
+            paraTriangle += `</br>`;
+        } 
+        paraTrianglePattern.innerHTML = paraTriangle
+    }
 }
